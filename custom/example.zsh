cd6d9a8d9f382ec0c3a9807f238b357cfc4d4cb5
Lindolfo 'Lorn' Rodrigues <lorn@lornlab.org>
2011-09-25 13:27:40 -0300

    ssh-reagent ftw

--- custom/example.zsh
+++ custom/example.zsh
@@ -36,3 +36,15 @@ alias gcl='git clone'
 irc() {
   ssh -A lorn@lornlab.org
 }
+
+ssh-reagent () {
+         for agent in /tmp/ssh-*/agent.*; do
+                export SSH_AUTH_SOCK=$agent
+                if ssh-add -l 2>&1 > /dev/null; then
+                        echo Found working SSH Agent:
+                        ssh-add -l
+                        return
+                fi
+        done
+        echo Cannot find ssh agent - maybe you should reconnect and forward it?
+}
